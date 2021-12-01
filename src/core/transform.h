03aa12a8fbaad2210c0fe870e14c27d31da828fd
peng00bo00 <pengbo_tongji@126.com>
2021-12-01 19:43:14 +0400

    update quaternion

--- src/core/transform.h
+++ src/core/transform.h
@@ -6,3 +6,14 @@
 #ifndef PBRT_CORE_TRANSFORM_H
 #define PBRT_CORE_TRANSFORM_H
 
+// core/transform.h*
+#include "pbrt.h"
+#include "stringprint.h"
+#include "geometry.h"
+#include "quaternion.h"
+
+namespace pbrt {
+
+}  // namespace pbrt
+
+#endif  // PBRT_CORE_TRANSFORM_H
