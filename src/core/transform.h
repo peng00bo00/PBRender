d823cf1fed60d107dc66d53ea027f67ee763a7f4
peng00bo00 <pengbo_tongji@126.com>
2021-12-01 20:02:53 +0400

    Update transform.h

--- src/core/transform.h
+++ src/core/transform.h
@@ -25,8 +25,9 @@ struct Matrix4x4 {
 
     Matrix4x4(Float mat[4][4]);
 
-    Matrix4x4(Float t00, Float t01, Float t02, Float t03, Float t10, Float t11,
-              Float t12, Float t13, Float t20, Float t21, Float t22, Float t23,
+    Matrix4x4(Float t00, Float t01, Float t02, Float t03, 
+              Float t10, Float t11, Float t12, Float t13, 
+              Float t20, Float t21, Float t22, Float t23,
               Float t30, Float t31, Float t32, Float t33);
     
     bool operator==(const Matrix4x4 &m2) const {
