1be43ac64a7830416c6984d9031da73be4897549
peng00bo00 <pengbo_tongji@126.com>
2021-12-02 15:14:06 +0400

    Update interaction.h

--- src/core/interaction.h
+++ src/core/interaction.h
@@ -28,32 +28,40 @@ struct Interaction {
           wo(Normalize(wo)),
           n(n),
           mediumInterface(mediumInterface) {}
+    
     bool IsSurfaceInteraction() const { return n != Normal3f(); }
+
     Ray SpawnRay(const Vector3f &d) const {
         Point3f o = OffsetRayOrigin(p, pError, n, d);
         return Ray(o, d, Infinity, time, GetMedium(d));
     }
+
     Ray SpawnRayTo(const Point3f &p2) const {
         Point3f origin = OffsetRayOrigin(p, pError, n, p2 - p);
         Vector3f d = p2 - p;
         return Ray(origin, d, 1 - ShadowEpsilon, time, GetMedium(d));
     }
+
     Ray SpawnRayTo(const Interaction &it) const {
         Point3f origin = OffsetRayOrigin(p, pError, n, it.p - p);
         Point3f target = OffsetRayOrigin(it.p, it.pError, it.n, origin - it.p);
         Vector3f d = target - origin;
         return Ray(origin, d, 1 - ShadowEpsilon, time, GetMedium(d));
     }
+
     Interaction(const Point3f &p, const Vector3f &wo, Float time,
                 const MediumInterface &mediumInterface)
         : p(p), time(time), wo(wo), mediumInterface(mediumInterface) {}
+    
     Interaction(const Point3f &p, Float time,
                 const MediumInterface &mediumInterface)
         : p(p), time(time), mediumInterface(mediumInterface) {}
+    
     bool IsMediumInteraction() const { return !IsSurfaceInteraction(); }
     const Medium *GetMedium(const Vector3f &w) const {
         return Dot(w, n) > 0 ? mediumInterface.outside : mediumInterface.inside;
     }
+
     const Medium *GetMedium() const {
         CHECK_EQ(mediumInterface.inside, mediumInterface.outside);
         return mediumInterface.inside;
@@ -73,9 +81,11 @@ class MediumInteraction : public Interaction {
   public:
     // MediumInteraction Public Methods
     MediumInteraction() : phase(nullptr) {}
+    
     MediumInteraction(const Point3f &p, const Vector3f &wo, Float time,
                       const Medium *medium, const PhaseFunction *phase)
         : Interaction(p, wo, time, medium), phase(phase) {}
+    
     bool IsValid() const { return phase != nullptr; }
 
     // MediumInteraction Public Data
@@ -93,13 +103,16 @@ class SurfaceInteraction : public Interaction {
                        const Normal3f &dndu, const Normal3f &dndv, Float time,
                        const Shape *sh,
                        int faceIndex = 0);
+    
     void SetShadingGeometry(const Vector3f &dpdu, const Vector3f &dpdv,
                             const Normal3f &dndu, const Normal3f &dndv,
                             bool orientationIsAuthoritative);
+    
     void ComputeScatteringFunctions(
         const RayDifferential &ray, MemoryArena &arena,
         bool allowMultipleLobes = false,
         TransportMode mode = TransportMode::Radiance);
+    
     void ComputeDifferentials(const RayDifferential &r) const;
     Spectrum Le(const Vector3f &w) const;
 
