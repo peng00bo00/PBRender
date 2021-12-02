fa20c345358c44b6d03b9897171fee400eaeb94c
peng00bo00 <pengbo_tongji@126.com>
2021-12-02 20:06:11 +0400

    update shape

--- src/shapes/sphere.cpp
+++ src/shapes/sphere.cpp
@@ -10,7 +10,7 @@ namespace pbrt {
 // Sphere Method Definitions
 Bounds3f Sphere::ObjectBound() const {
     return Bounds3f(Point3f(-radius, -radius, zMin),
-                    Point3f(radius, radius, zMax));
+                    Point3f( radius,  radius, zMax));
 }
 
 bool Sphere::Intersect(const Ray &r, Float *tHit, SurfaceInteraction *isect,
