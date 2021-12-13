3a87c1ca21d09a2478bf1b461218b50549063cab
peng00bo00 <pengbo_tongji@126.com>
2021-12-13 10:42:10 +0400

    rebuild from beginning

---
+++ src/core/PBRender.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace PBRender {
+
+// geometry
+template <typename T>
+class Vector2;
+
+template <typename T>
+class Vector3;
+
+typedef Vector2<float> Vector2f;
+typedef Vector2<int> Vector2i;
+typedef Vector3<float> Vector3f;
+typedef Vector3<int> Vector3i;
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "PBRender.h"
+
+using namespace PBRender;
+
+int main(int argc, char *argv[]) {
+    std::cout << "Finished!" << std::endl;
+}
