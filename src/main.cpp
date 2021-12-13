44e01e9a4673787a299a7406dc01ae65b39a3c4f
peng00bo00 <pengbo_tongji@126.com>
2021-12-13 16:25:49 +0400

    update transform

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "PBRender.h"
 #include "geometry.h"
+#include "transform.h"
 
 using namespace PBRender;
 
@@ -9,4 +10,7 @@ int main(int argc, char *argv[]) {
     auto z = Vector3f(-1.0, 2, 3.4);
     std::cout << x*2 << std::endl;
     std::cout << y/2.0 << std::endl;
+
+    auto m = Matrix4x4();
+    std::cout << m << std::endl;
 }
