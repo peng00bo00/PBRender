42334220fc5229fa7e128713a64a6610b2275d13
peng00bo00 <pengbo_tongji@126.com>
2021-12-13 17:02:52 +0400

    update shape

--- src/core/shape.cpp
+++ src/core/shape.cpp
@@ -1,5 +1,20 @@
 #include "shape.h"
 
 namespace PBRender {
-    
+
+long long Shape::nShapesCreated = 0;
+
+Shape::Shape(const Transform *ObjectToWorld, const Transform *WorldToObject,
+             bool reverseOrientation)
+    : ObjectToWorld(ObjectToWorld),
+      WorldToObject(WorldToObject),
+      reverseOrientation(reverseOrientation),
+      transformSwapsHandedness(ObjectToWorld->SwapsHandedness()) {
+    ++nShapesCreated;
+}
+
+Shape::~Shape() {}
+
+Bounds3f Shape::WorldBound() const { return (*ObjectToWorld)(ObjectBound()); }
+
 }
