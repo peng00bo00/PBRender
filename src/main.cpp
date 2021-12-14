05fb23da849bd38c9adcc6d889ba0af0f5c49a3a
peng00bo00 <pengbo_tongji@126.com>
2021-12-14 16:42:42 +0400

    add stbiw

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,22 @@
 #include "transform.h"
 #include "shapes/sphere.h"
 
+#include <stb_image_write.h>
+
 using namespace PBRender;
+
+void test_rainbow() {
+    std::vector<char> buf(512 * 512 * 3);
+    for (int j = 0; j < 512; j++) {
+        for (int i = 0; i < 512; i++) {
+            buf[(j * 512 + i) * 3 + 0] = i / 2;
+            buf[(j * 512 + i) * 3 + 1] = j / 2;
+            buf[(j * 512 + i) * 3 + 2] = 0;
+        }
+    }
+    stbi_write_png("rainbow.png", 512, 512, 3, buf.data(), 0);
+}
+
 
 void renderFrame() {
     Transform sphereT_Object2World, sphereT_World2Object;
@@ -15,6 +30,7 @@ void renderFrame() {
     Vector3f up( 0.0 , 1.0 , 0.0 ) ;
 
     Transform lookat = LookAt ( eye , look , up ) ;
+    test_rainbow();
 
 }
 
