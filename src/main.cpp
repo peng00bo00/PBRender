189f86d9614f366c5076e7e114c6cd770ec5bd95
peng00bo00 <pengbo_tongji@126.com>
2024-06-20 19:26:26 +0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,13 @@
 #include <iostream>
 #include <ctime>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+
 using Point3f = PBRender::Point3f;
 using Vector3i= PBRender::Vector3i;
 using Vector3f= PBRender::Vector3f;
@@ -19,92 +26,175 @@ int main() {
 
     // create engine instance
     auto engine = std::make_unique<PBRender::Engine>();
-
-    std::cout << "initialize Scene..." << std::endl;
-    // auto scene = std::make_unique<PBRender::Scene>(engine->device);
     auto scene = engine->scene;
 
-    // std::cout << "adding random triangles to the Scene..." << std::endl;
-    size_t N = 10000;
-    uint geomID;
+    // add a sphere
+    Point3f center(Point3f(0.0f, 0.0f, 1.0f));
+    float radius = 0.5f;
+    scene->AddSphere(center, radius);
+    scene->FinishScene();
+
+    // create a film
+    const size_t H = 1024;
+    const size_t W = 1024;
     
-    std::vector<Point3f> vertices;
-    std::vector<Vector3i> indices;
+    const float xmin = -1.0f;
+    const float xmax =  1.0f;
+    const float ymin = -1.0f;
+    const float ymax =  1.0f;
 
-    vertices.reserve(N);
-    indices.reserve(N);
+    std::vector<Vector3f> film(H*W, Vector3f(0.f,0.f,0.f));
 
-    for (size_t i=0; i<N; ++i)
-    {
-        const float xx = float(drand48())-0.5;
-        const float yy = float(drand48())-0.5;
-        const float zz = float(drand48())-0.5;
+    // rasterization
+    for (size_t i = 0; i < H; ++i)
+    {   
+        float ty = (i+0.5f) / H;
+        float yy = PBRender::Lerp(ty, ymin, ymax);
 
-        vertices.emplace_back(xx*100.f, yy*100.f, zz*100.f);
-        indices.emplace_back(i*3, i * 3 + 1,i * 3 + 2);
+        for (size_t j = 0; j < W; ++j)
+        {
+            float tx = (j+0.5f) / W;
+            float xx = PBRender::Lerp(tx, xmin, xmax);
+
+            // initialize a ray
+            RTCRayHit rayhit;
+            rayhit.ray.org_x = xx;
+            rayhit.ray.org_y = yy;
+            rayhit.ray.org_z = 0.f;
+            rayhit.ray.dir_x = 0.f;
+            rayhit.ray.dir_y = 0.f;
+            rayhit.ray.dir_z = 1.f;
+            rayhit.ray.tnear = 0;
+            rayhit.ray.tfar = std::numeric_limits<float>::infinity();
+            rayhit.ray.mask = -1;
+            rayhit.ray.flags = 0;
+            rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
+            rayhit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
+
+            // cast the ray to scene
+            scene->RayHit(&rayhit);
+            if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID) {
+                // // shading with normal
+                // float nx = rayhit.hit.Ng_x * 0.5f + 0.5f;
+                // float ny = rayhit.hit.Ng_y * 0.5f + 0.5f;
+                // float nz = rayhit.hit.Ng_z * 0.5f + 0.5f;
+
+                // film[i*H+j] = Vector3f(nx,ny,nz);
+
+                // shading with depth
+                float depth = rayhit.ray.tfar;
+                // std::cout << depth << std::endl;
+                film[i*H+j] = Vector3f(depth, depth, depth);
+            }
+        }
     }
 
-    geomID = scene->AddTriMesh(vertices, indices);
-    std::cout << "Triangle Mesh ID: " << geomID << std::endl;
+    // write to image
+    auto buf = std::vector<char>();
+    buf.resize(3 * film.size());
 
-    // for (size_t i=0; i<10; ++i)
+    for (size_t i = 0; i < film.size(); ++i)
+    {
+        buf[3 * i + 0] = (uint8_t) PBRender::Clamp(255.f * film[i].x + 0.5f, 0.f, 255.f);
+        buf[3 * i + 1] = (uint8_t) PBRender::Clamp(255.f * film[i].y + 0.5f, 0.f, 255.f);
+        buf[3 * i + 2] = (uint8_t) PBRender::Clamp(255.f * film[i].z + 0.5f, 0.f, 255.f);
+    }
+
+    stbi_write_png("output.png", W, H, 3, buf.data(), 0);
+
+    // for (size_t i = 0; i < H; i++)
+    // {
+    //     for (size_t j = 0; j < W; j++)
+    //     {
+    //         std::cout << film[i*H+j].x;
+    //     }
+    //     std::cout << std::endl;
+        
+    // }
+    
+    
+    // write to ppm
+    
+
+    // // std::cout << "adding random triangles to the Scene..." << std::endl;
+    // size_t N = 10000;
+    // uint geomID;
+    
+    // std::vector<Point3f> vertices;
+    // std::vector<Vector3i> indices;
+
+    // vertices.reserve(N);
+    // indices.reserve(N);
+
+    // for (size_t i=0; i<N; ++i)
     // {
     //     const float xx = float(drand48())-0.5;
     //     const float yy = float(drand48())-0.5;
     //     const float zz = float(drand48())-0.5;
-    //     const float r  = float(drand48());
 
-    //     geomID = scene->AddSphere(Point3f(xx, yy, zz), r);
-    //     std::cout << "Sphere ID: " << geomID << std::endl;
+    //     vertices.emplace_back(xx*100.f, yy*100.f, zz*100.f);
+    //     indices.emplace_back(i*3, i * 3 + 1,i * 3 + 2);
     // }
 
-    // // scene->AddRandomTriangles();
-    // std::cout << "adding random triangles finished!" << std::endl;
+    // geomID = scene->AddTriMesh(vertices, indices);
+    // std::cout << "Triangle Mesh ID: " << geomID << std::endl;
 
-    std::cout << "commit scene..." << std::endl;
-    scene->FinishScene();
-    std::cout << "commit scene finished!" << std::endl;
+    // // for (size_t i=0; i<10; ++i)
+    // // {
+    // //     const float xx = float(drand48())-0.5;
+    // //     const float yy = float(drand48())-0.5;
+    // //     const float zz = float(drand48())-0.5;
+    // //     const float r  = float(drand48());
 
-    // double t1 = getSeconds();
+    // //     geomID = scene->AddSphere(Point3f(xx, yy, zz), r);
+    // //     std::cout << "Sphere ID: " << geomID << std::endl;
+    // // }
 
-    // // castRay(*scene, 1.00f, 1.00f, -1, 0, 0, 1);
-    for (size_t i = 0; i < 100; i++)
-    {
-        // random origin
-        const float ox = float(drand48())-0.5;
-        const float oy = float(drand48())-0.5;
-        const float oz = float(drand48())-0.5;
+    // // // scene->AddRandomTriangles();
+    // // std::cout << "adding random triangles finished!" << std::endl;
 
-        // random dir
-        const float dx = float(drand48())-0.5;
-        const float dy = float(drand48())-0.5;
-        const float dz = float(drand48())-0.5;
+    // std::cout << "commit scene..." << std::endl;
+    // scene->FinishScene();
+    // std::cout << "commit scene finished!" << std::endl;
 
-        Vector3f dir(dx, dy, dz);
-        dir = Normalize(dir);
+    // // double t1 = getSeconds();
+    // for (size_t i = 0; i < 100; i++)
+    // {
+    //     // random origin
+    //     const float ox = float(drand48())-0.5;
+    //     const float oy = float(drand48())-0.5;
+    //     const float oz = float(drand48())-0.5;
+
+    //     // random dir
+    //     const float dx = float(drand48())-0.5;
+    //     const float dy = float(drand48())-0.5;
+    //     const float dz = float(drand48())-0.5;
+
+    //     Vector3f dir(dx, dy, dz);
+    //     dir = Normalize(dir);
         
-        RTCRayHit rayhit;
-        rayhit.ray.org_x = ox;
-        rayhit.ray.org_y = oy;
-        rayhit.ray.org_z = oz;
-        rayhit.ray.dir_x = dx;
-        rayhit.ray.dir_y = dy;
-        rayhit.ray.dir_z = dz;
-        rayhit.ray.tnear = 0;
-        rayhit.ray.tfar = std::numeric_limits<float>::infinity();
-        rayhit.ray.mask = -1;
-        rayhit.ray.flags = 0;
-        rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
-        rayhit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
+    //     RTCRayHit rayhit;
+    //     rayhit.ray.org_x = ox;
+    //     rayhit.ray.org_y = oy;
+    //     rayhit.ray.org_z = oz;
+    //     rayhit.ray.dir_x = dx;
+    //     rayhit.ray.dir_y = dy;
+    //     rayhit.ray.dir_z = dz;
+    //     rayhit.ray.tnear = 0;
+    //     rayhit.ray.tfar = std::numeric_limits<float>::infinity();
+    //     rayhit.ray.mask = -1;
+    //     rayhit.ray.flags = 0;
+    //     rayhit.hit.geomID = RTC_INVALID_GEOMETRY_ID;
+    //     rayhit.hit.instID[0] = RTC_INVALID_GEOMETRY_ID;
 
-        scene->RayHit(&rayhit);
-        if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID) {
-            printf("Ray %zu finds intersection on geometry %d, primitive %d.\n", 
-            i,
-            rayhit.hit.geomID,
-            rayhit.hit.primID);
-        } else {
-            printf("Ray %zu does not find any intersection.\n\n", i);
-        }
-    }
+    //     scene->RayHit(&rayhit);
+    //     if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID) {
+    //         printf("Ray %zu finds intersection on geometry %d, primitive %d.\n", 
+    //         i,
+    //         rayhit.hit.geomID,
+    //         rayhit.hit.primID);
+    //     } else {
+    //         printf("Ray %zu does not find any intersection.\n\n", i);
+    //     }
+    // }
 }
