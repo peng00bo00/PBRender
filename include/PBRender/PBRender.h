70f04c1c5c4c126f9e59fa70fc84d2dfab123eaa
peng00bo00 <pengbo_tongji@126.com>
2024-07-06 15:00:57 +0400

    Update PBRender.h

--- include/PBRender/PBRender.h
+++ include/PBRender/PBRender.h
@@ -9,19 +9,13 @@
 // core
 #include <PBRender/core/common.h>
 
-#include <PBRender/core/container.h>
 #include <PBRender/core/engine.h>
-#include <PBRender/core/spectrum.h>
 #include <PBRender/core/film.h>
-#include <PBRender/core/math.h>
-#include <PBRender/core/scene.h>
-#include <PBRender/core/vecmath.h>
 #include <PBRender/core/transform.h>
 #include <PBRender/core/camera.h>
 #include <PBRender/core/ray.h>
-#include <PBRender/core/rng.h>
-#include <PBRender/core/sampler.h>
-#include <PBRender/core/sampling.h>
+#include <PBRender/core/scene.h>
+#include <PBRender/core/spectrum.h>
 
 // cameras
 #include <PBRender/cameras/perspective.h>
